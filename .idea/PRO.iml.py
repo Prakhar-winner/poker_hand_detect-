7b560cdd280381211ed823ae398bd4b032d528c7
Prakhar-winner <bs23b038@smail.iitm.ac.in>
2025-06-18 10:46:33 +0530

    Add files via upload

---
+++ .idea/PRO.iml.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 18 10:36:44 2025
+
+@author: prakh
+"""
+
+<?xml version="1.0" encoding="UTF-8"?>
+<module type="PYTHON_MODULE" version="4">
+  <component name="NewModuleRootManager">
+    <content url="file://$MODULE_DIR$">
+      <excludeFolder url="file://$MODULE_DIR$/venv" />
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+  </component>
+</module>
